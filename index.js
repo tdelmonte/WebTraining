08383d8ad1f13308bc197416fc445fa1af325928
tdelmonte <tdelmonte@gmail.com>
2019-10-06 10:32:10 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,2 @@
+const sentence =`My name is ${name}, and I am ${age} years old.`
+console.log(sentence);
---
+++ index1.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+const name = 'Bill'
+const age = 35
+
+const sentence =`My name is ${name}, and I am ${age} years old.`
+console.log(sentence);
